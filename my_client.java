7ff04c934d0d6d42575903b0610826eb3241392d
Kajol Murmu <120392831+Kajol-m@users.noreply.github.com>
2024-01-12 21:43:52 +0530

    Update my_client.java

--- my_client.java
+++ my_client.java
@@ -1,40 +1,46 @@
-package cn_project_10;
-
+package cn_project10;
 import java.util.*;
 import java.io.*;
 import java.net.*;
+
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
-public class my_client {
+public class Client {
 	public static void main(String[] args)throws UnknownHostException,IOException{
 		try {
-			Socket s=new Socket("localhost",6666);
+			Socket s=new Socket("localhost",6667);
 			
-			File filesend=new File("C:\\Users\\kajol\\Downloads\\Hello.txt");
-			String fileName = filesend.getName();
-	        long fileSize = filesend.length();
-			
-			DataOutputStream outputStream = new DataOutputStream(s.getOutputStream());
-			outputStream.writeUTF(fileName);
-	        outputStream.writeLong(fileSize);
-	        
-			FileInputStream fileInputStream= new FileInputStream(filesend);
-			BufferedInputStream bin = new BufferedInputStream(fileInputStream);
-	        byte[] buffer = new byte[4096];
-	        int bytesRead;
-	        while ((bytesRead = bin.read(buffer)) != -1) {
-	            outputStream.write(buffer, 0, bytesRead);
-	        }
-	        
-	        bin.close();
-	        outputStream.close();
-	        s.close();
-	        System.out.println("File sent successfully!");
-			
-			
+			DataInputStream inputStream = new DataInputStream(s.getInputStream());
+			 String fileName = inputStream.readUTF();
+	         long fileSize = inputStream.readLong();
+	         
+	         //receive file from server
+			 FileOutputStream fileOutputStream = new FileOutputStream(fileName);//writes file content
+	         BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(fileOutputStream);
+
+	            // Receive file contents in chunks
+	            byte[] buffer = new byte[4096];
+	            int bytesRead;
+	            while ((bytesRead = inputStream.read(buffer)) != -1) {
+	                bufferedOutputStream.write(buffer, 0, bytesRead);
+	          
+	            }
+
+	            // Close streams and socket
+	            System.out.println(fileName+" File received successfully!");
+	            bufferedOutputStream.close();
+	            inputStream.close();
+	            s.close();
+	            
+	       
+	            
+					
 		}
-		 catch (Exception e) {
+		catch(IOException e) {
 			e.printStackTrace();
 		}
 	}
