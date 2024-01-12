b5f33a2db0617485daeba2203c8c6e56e1831aef
Kajol Murmu <120392831+Kajol-m@users.noreply.github.com>
2024-01-12 21:43:33 +0530

    Update clientGUI.java

--- clientGUI.java
+++ clientGUI.java
@@ -1,41 +1,41 @@
-package cn_project_10;
+package cn_project10;
 
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class clientGUI extends JFrame implements ActionListener {
-	private my_client client;
+public class clientGUI extends JFrame implements ActionListener {   //extends jframe and ActionListener to use its component
+	private Client client;
 
-	public clientGUI(my_client my_client) {
-		super("my_client");
-		this.client=my_client;
+	public clientGUI(Client Client) {    //create constructor
+		super("Client");                 //extend Client "parent class" through super
+		this.client=Client;
 		
-		JPanel panel=new JPanel();
+		JPanel panel=new JPanel();  //create Panel
 		
-		panel.setBorder(BorderFactory.createEmptyBorder(50,50,10,50));
-		panel.setLayout(new GridLayout(0,1));
+		panel.setBorder(BorderFactory.createEmptyBorder(50,50,10,50)); //set border to panel
+		panel.setLayout(new GridLayout(0,1));                          //set layout to panel
 		
-		JTextField reqHelp=new JTextField();
-		reqHelp.setBounds(100,100,100,100);
+		JTextField reqHelp=new JTextField();                          //TextField to add text
+		reqHelp.setBounds(100,100,100,100);                           //set bounds border etc. 
 		reqHelp.setEditable(false);
         
 		
-		JButton button=new JButton("Request for Help");
-		button.addActionListener(new ActionListener() {
+		JButton button=new JButton("Request for Help");                //create button
+		button.addActionListener(new ActionListener() {                //create and add ActionListener to  button
 			@Override
-			public void actionPerformed(ActionEvent e) {
+			public void actionPerformed(ActionEvent e) {               //Action to be performed through actionListener
 				reqHelp.setText("Server:A computer program or dedicated hardware that provides resources and services to other computers (clients) over a network.                                 Client:A computer program or device that requests and utilizes resources and services provided by a server over a network.");
-				
+				//adding text
 			}
 		});
-		panel.add(button);
-		//panel.add(reqHelp);
-		panel.add(new JScrollPane(reqHelp), BorderLayout.SOUTH);
+		panel.add(button);  //add button to panel
 		
-		add(panel,BorderLayout.CENTER);
+		panel.add(new JScrollPane(reqHelp), BorderLayout.SOUTH);   //add scroll feature to panel
+		
+		add(panel,BorderLayout.CENTER);                            //add panel to frame
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setTitle("HELP!!");
+		setTitle("HELP!!");                                         //set Title
 		pack();
 		setVisible(true);
 		
@@ -45,8 +45,8 @@ public class clientGUI extends JFrame implements ActionListener {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		try {
-			my_client client=new my_client();
-			clientGUI a=new clientGUI(client);
+			Client client=new Client();                 //create Client object
+			clientGUI a=new clientGUI(client);          //creates 
 		}
 		catch(Exception e) {
 			e.printStackTrace();
