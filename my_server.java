53a37d7bea8a153b6f33af3f879d963a73b14af5
Kajol Murmu <120392831+Kajol-m@users.noreply.github.com>
2024-01-12 21:41:06 +0530

    Update my_server.java

--- my_server.java
+++ my_server.java
@@ -1,4 +1,4 @@
-package cn_project_10;
+package cn_project10;
 
 import java.util.*;
 import java.io.*;
@@ -7,43 +7,48 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
 
-public class my_server{
+public class Server {
 	
-	
+
 	public static void main(String[] args)throws UnknownHostException,IOException {
 		try {
-			ServerSocket ss=new ServerSocket(6666);
+			ServerSocket ss=new ServerSocket(6667);
+			while(true) {
 			Socket s=ss.accept();
 			System.out.println("Connected");
 			
-			 DataInputStream inputStream = new DataInputStream(s.getInputStream());
-			 String fileName = inputStream.readUTF();
-	         long fileSize = inputStream.readLong();
-			 FileOutputStream fileOutputStream = new FileOutputStream("received_" + fileName);
-	         BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(fileOutputStream);
-
-	            // Receive file contents in chunks
-	            byte[] buffer = new byte[4096];
-	            int bytesRead;
-	            long totalBytesRead = 0;
-	            while ((bytesRead = inputStream.read(buffer)) != -1) {
-	                bufferedOutputStream.write(buffer, 0, bytesRead);
-	                totalBytesRead += bytesRead;
-	                System.out.println("Received " + totalBytesRead + " bytes...");
-	            }
-
-	            // Close streams and socket
-	            bufferedOutputStream.close();
-	            inputStream.close();
-	            s.close();
-	            System.out.println("File received successfully!");
-					}
+			
+			File filesend=new File("C:\\Users\\kajol\\Downloads\\Hello.txt");
+			String fileName = filesend.getName();
+	        long fileSize = filesend.length();
+			
+			DataOutputStream outputStream = new DataOutputStream(s.getOutputStream());
+			outputStream.writeUTF(fileName);
+	        outputStream.writeLong(fileSize);
+	        
+	        //sends file to client
+			FileInputStream fileInputStream= new FileInputStream(filesend);//reads file content
+			BufferedInputStream bin = new BufferedInputStream(fileInputStream);
+	        byte[] buffer = new byte[4096];
+	        int bytesRead;
+	        while ((bytesRead = bin.read(buffer)) != -1) {
+	            outputStream.write(buffer, 0, bytesRead);
+	        }
+	        
+	        bin.close();
+	        outputStream.close();
+	        s.close();
+	        System.out.println("File sent successfully!");
+	        
+	        
+	        
+		}}
 		catch (IOException e) {
 			e.printStackTrace();
 		}
-		
 	}
-
-
 }
+
+
